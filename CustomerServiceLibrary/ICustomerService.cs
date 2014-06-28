acf0fae87b5cd890e291235f768ec4cb3ef29d33
ganeshn83 <ganesh_nataraj@hotmail.com>
2014-06-28 12:09:29 +0530

    committing changes

--- CustomerServiceLibrary/ICustomerService.cs
+++ CustomerServiceLibrary/ICustomerService.cs
@@ -7,6 +7,8 @@ using System.ServiceModel.Web;
 using System.Text;
 using Domain;
 
+
+
 namespace CustomerServiceLibrary
 {
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
